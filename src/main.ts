52fe45a48383719ee1982946984944747613cc19
nnnozimjon <iamnozimjon@gmail.com>
2025-05-15 17:24:14 +0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
     // Enable CORS
     app.enableCors({
-      origin: ['poputi.tj', 'test.poputi.tj', 'paputi.tj'], // Allow all origins (Change this to specific origins in production)
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: 'Content-Type, Authorization',
+        origin: [
+            'https://poputi.tj',
+            'https://test.poputi.tj',
+            'https://paputi.tj',
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Authorization',
     });
 
-  app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(new ValidationPipe());
 
-   // Set global prefix for all routes
-   app.setGlobalPrefix('api/client'); // This will make all routes start with "/api/client"
+    // Set global prefix for all routes
+    app.setGlobalPrefix('api/client'); // This will make all routes start with "/api/client"
 
-  await app.listen(process.env.PORT || 3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
